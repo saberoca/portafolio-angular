2f99464c108f0913516beda170cdd932ddc85c03
Santos Bertoni <saberoca@gmail.com>
2019-01-26 16:52:15 -0600

    Template rutas y animaciones 1

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,18 +8,30 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
 
   info: InfoPagina = {};
+  equipo: any[] = [];
   cargada = false;
 
   constructor( private http: HttpClient) {
-    //leer el archivo JSON
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+  private cargarInfo(){
+    //leer el archivo JSON
     this.http.get('assets/data/data-pagina.json').subscribe( (resp: InfoPagina) => {
       
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
-
       });
-
   }
+
+  private cargarEquipo(){
+    //leer el archivo JSON
+    this.http.get('https://angular-html-83ec5.firebaseio.com/equipo.json').subscribe( (resp: any[]) => {
+      
+      this.equipo = resp;
+      console.log(resp);
+      });
+  }
+
 }
